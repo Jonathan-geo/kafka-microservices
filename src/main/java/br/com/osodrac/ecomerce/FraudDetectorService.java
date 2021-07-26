890e238cc43f9724b56383d227ad6e60afb10b31
jonathan-geo <jonathan.geotecs@gmail.com>
2021-07-26 18:10:57 -0300

    kafka - KafkaProducer

---
+++ src/main/java/br/com/osodrac/ecomerce/FraudDetectorService.java
@@ -0,0 +1,9 @@
+package br.com.osodrac.ecomerce;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        //var consumer = new KafkaConsumer<String, String>(properties);
+    }
+}
---
+++ src/main/java/br/com/osodrac/ecomerce/NewOrderMain.java
@@ -0,0 +1,33 @@
+package br.com.osodrac.ecomerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String,String>(properties());
+        var value = "{id:'44335',product:'smart-tv'}";
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("success: topico " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties(){
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
